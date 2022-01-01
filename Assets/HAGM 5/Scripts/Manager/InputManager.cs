83d0a885584f61a86ea84437f53f12c89fc362ec
Rendyl <r.latbi@laposte.net>
2022-01-01 22:53:49 +0100

    Started Cursor

--- Assets/HAGM 5/Scripts/Manager/InputManager.cs
+++ Assets/HAGM 5/Scripts/Manager/InputManager.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    void OnMouseEnter ()
     {
-        
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseExit ()
     {
-        
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
     }
 }
