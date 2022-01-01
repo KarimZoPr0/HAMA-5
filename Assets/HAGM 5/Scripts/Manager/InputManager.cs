9ef69fc4c90a36830194389c7be3baff1db07520
Rendyl <r.latbi@laposte.net>
2022-01-01 23:51:19 +0100

    Created Prefabs onto Main Scene and added Cursor In Game

--- Assets/HAGM 5/Scripts/Manager/InputManager.cs
+++ Assets/HAGM 5/Scripts/Manager/InputManager.cs
@@ -1,20 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class InputManager : MonoBehaviour
+public class InputManager : Singleton<InputManager>
 {
-    public Texture2D cursorTexture;
-    public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
-    void OnMouseEnter ()
-    {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-    }
+	[Title("Cursor")]
+	[SerializeField] private Texture2D m_handBaseTexture;
+	[SerializeField] private Texture2D m_handInteractTexture;
+	[SerializeField] private Texture2D m_handGrabTexture;
 
-    void OnMouseExit ()
-    {
-        Cursor.SetCursor(null, Vector2.zero, cursorMode);
-    }
+	private CursorState m_cursorState;
+	public static CursorState CursorStatus
+	{
+		get => Instance.m_cursorState;
+		set
+		{
+			Instance.m_cursorState = value;
+
+			Cursor.visible = CursorStatus != CursorState.None;
+
+			switch (CursorStatus)
+			{
+				case CursorState.Cursor:
+					Cursor.SetCursor(Instance.m_handBaseTexture, m_hotSpot, m_cursorMode);
+					break;
+				case CursorState.Interact:
+					Cursor.SetCursor(Instance.m_handInteractTexture, m_hotSpot, m_cursorMode);
+					break;
+				case CursorState.Grab:
+					Cursor.SetCursor(Instance.m_handGrabTexture, m_hotSpot, m_cursorMode);
+					break;
+			}
+		}
+	}
+
+	public enum CursorState
+	{
+		None,
+		Cursor,
+		Interact,
+		Grab,
+	}
+
+	private static CursorMode m_cursorMode = CursorMode.Auto;
+	private static Vector2 m_hotSpot = new Vector2(4f, 0f);
+
+	public void Start ()
+	{
+		CursorStatus = CursorState.Cursor;
+	}
+
+	void OnMouseEnter ()
+	{
+		Cursor.SetCursor(m_handBaseTexture, m_hotSpot, m_cursorMode);
+	}
+
+	void OnMouseExit ()
+	{
+		Cursor.SetCursor(null, Vector2.zero, m_cursorMode);
+	}
+
+	private void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+			CursorStatus = CursorState.Cursor;
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+			CursorStatus = CursorState.None;
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+			CursorStatus = CursorState.Interact;
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+			CursorStatus = CursorState.Grab;
+	}
 }
